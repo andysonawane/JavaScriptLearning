dafdb87086ea9030e25fa2f283ecf099bda1519b
andysonawane <andysonawane14@gmail.com>
2022-12-08 20:39:17 +0530

    variableAssi
    variableAssi

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName; // Variable Declaration
+myName = "Anand Sonawane";// Variable initialization
+console.log(myName);
+
+var myFedName="Mahesh Patil" //
+console.log(myFedName);
+
+myFedName="kumbhar";
+console.log(myFedName);
---
+++ 01_variable_assig.js
@@ -0,0 +1,18 @@
+
+myFullName="Anand Sonawane"
+console.log(myFullName);
+myFullName="Anand Parmeshwar Sonawane";
+console.log(myFullName);
+myLove="I LOve Only JS";
+console.log(myLove);
+
+var firstName ="Anand";
+var lastName ="Sonawane";
+var age ="28";
+var address ="Sangola";
+var pin_code ="413308";
+var gender ="male";
+var is_married ="yes";
+
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
+
