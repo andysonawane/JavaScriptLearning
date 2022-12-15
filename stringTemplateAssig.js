db3635f449973c91de08b0d8f699aacefc383604
andysonawane <andysonawane14@gmail.com>
2022-12-15 07:05:50 +0530

    stringTempalAssignment
    stringTempalAssignment

---
+++ stringTemplateAssig.js
@@ -0,0 +1,19 @@
+
+var companyName= ` My Dream Company is name "TCS"`
+console.log(`1) My Dream Company is name "TCS"`);
+
+console.log("######################02.1######################");
+
+var stringAssignment = function() 
+{Hobbies1=`reading new bookes`
+var Hobbies2=`playing cricket`
+var Hobbies3=`playing basket ball`
+
+console.log(`2.1)(using variable substitution) My hobbies are: ${Hobbies1} ${Hobbies2} ${Hobbies3}`);
+
+
+console.log(`2.2)(using string teemplate) My hobbies are: ${Hobbies1.concat(Hobbies2,Hobbies3)}`);
+
+}
+
+stringAssignment();
